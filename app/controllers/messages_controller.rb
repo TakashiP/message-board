5579b5bb4e7e6b7374fd7b7f4c8a97cb8110a9e1
TakashiP <masai.takashi@gmail.com>
2015-11-26 08:05:17 +0000

    age column added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,7 @@ before_action:set_message, only: [:edit,:update,:destroy]
   
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   def set_message
     @message = Message.find(params[:id])
--- db/migrate/20151119073622_create_messages.rb
+++ db/migrate/20151119073622_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.string :name
       t.string :body
+      t.number :integer
 
       t.timestamps null: false
     end
