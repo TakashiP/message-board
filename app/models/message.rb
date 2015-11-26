5a3e2f31c63fbdd07ce7e855cb8ea6f8f4458010
TakashiP <masai.takashi@gmail.com>
2015-11-26 08:32:40 +0000

    age added

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,6 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 年齢０歳以上
-  validates :age , greater_than_or_equal_to:0
+  validates :age , numericality: { greater_than_or_equal_to: 0 }
   
 end
